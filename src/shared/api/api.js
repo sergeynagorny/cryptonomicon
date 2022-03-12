6ae0fa313893e1a11e5ef50c86c827caf4cbae4d
sergeynagorny <sergey.s.nagorny@gmail.com>
2022-03-12 19:54:19 +0300

    feat(api): subscribe to tickers

--- src/shared/api/api.js
+++ src/shared/api/api.js
@@ -1,5 +1,23 @@
-export const getTickets = () => {
+export const getTickets = (tickerNames) => {
+  const tsyms = tickerNames.join(",");
+
   return fetch(
-    "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR"
+    `https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms=${tsyms}`
   ).then((r) => r.json());
+};
+
+let nextRequestTimeoutId;
+
+export const subscribeToTickers = (tickerNames, cb) => {
+  clearInterval(nextRequestTimeoutId);
+  if (!tickerNames.length) return;
+
+  getTickets(tickerNames)
+    .then(cb)
+    .finally(() => {
+      nextRequestTimeoutId = setTimeout(
+        () => subscribeToTickers(tickerNames, cb),
+        1000
+      );
+    });
 };
